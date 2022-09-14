52890efa6943c74dff14fc5fff9137d63d7536bd
Marcel Barbosa <marcel.sby.br@gmail.com>
2022-09-13 23:57:35 -0300

    Initial commit

---
+++ src/main/java/com/marcelsby/coffeeshopacl/CoffeeShopAclApplication.java
@@ -0,0 +1,13 @@
+package com.marcelsby.coffeeshopacl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeShopAclApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CoffeeShopAclApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/marcelsby/coffeeshopacl/CoffeeShopAclApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelsby.coffeeshopacl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeShopAclApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
